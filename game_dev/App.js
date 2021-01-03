d88e66706fa7225e5f7522c9d68c55385384b778
Sean <seanskg72@gmail.com>
2021-01-03 16:38:35 +1100

    grid rendering

--- game_dev/App.js
+++ game_dev/App.js
@@ -2,38 +2,37 @@ import React, { useState } from 'react';
 import { Text, View, Button } from 'react-native';
 import styles from './Style.js';
 
+// Create 2D Object with specified length
 const createGrid = (length) => {
   let state = {}, counter = 0;
   for (let i = 0; i < length; i++) {
     let temp = [];
     for (let j = 0; j < length; j++) {
       temp.push({ key: counter, state: 0 });
-      counter++;
+      counter++; 
     }
     eval(`state.row${i} = temp`);
   }
   return state;
 }
 
-export default function App() {
-  let state = createGrid(5);
-  console.log(state);
+// Return 2D grid of buttons
+const makeGrid = (length) => {
+  let state = createGrid(length);
   return (
     <View style={styles.container}>
       {state.row0.map(element => {
-        return <View>
-          <Button title={`${element.key}`} key={`${element.key}`} />
-        </View>
+        return (
+          <View key={`${element.key}`}>
+            <Button title={`${element.key}`} />
+          </View>
+        )
       })}
     </View>
   )
 }
 
-
-// const [el, el_change] = useState("Welcome to this test app!");
-// return (
-//   <View style={styles.container}>
-//     <Text>{el}</Text>
-//     <Button title="push me!!!" onPress={() => el_change("PRESS HARDER!")}/>
-//   </View>
-// );
+// Rendering
+export default function App() {
+  return makeGrid(5);
+}
