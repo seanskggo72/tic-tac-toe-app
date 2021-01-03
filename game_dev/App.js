496a7a57da03120309532a29befa96abf8f57b1d
Sean <seanskg72@gmail.com>
2021-01-03 18:05:10 +1100

    file handling and layout changes + 2D array function complete

--- game_dev/App.js
+++ game_dev/App.js
@@ -1,36 +1,6 @@
 import React, { useState } from 'react';
-import { Text, View, Button } from 'react-native';
-import styles from './Style.js';
-
-// Create 2D Object with specified length
-const createGrid = (length) => {
-  let state = {}, counter = 0;
-  for (let i = 0; i < length; i++) {
-    let temp = [];
-    for (let j = 0; j < length; j++) {
-      temp.push({ key: counter, state: 0 });
-      counter++; 
-    }
-    eval(`state.row${i} = temp`);
-  }
-  return state;
-}
-
-// Return 2D grid of buttons
-const makeGrid = (length) => {
-  let state = createGrid(length);
-  return (
-    <View style={styles.container}>
-      {state.row0.map(element => {
-        return (
-          <View key={`${element.key}`}>
-            <Button title={`${element.key}`} />
-          </View>
-        )
-      })}
-    </View>
-  )
-}
+import { Text, View, StyleSheet, Button } from 'react-native';
+import makeGrid from './Grid';
 
 // Rendering
 export default function App() {
---
+++ game_dev/Grid.js
@@ -0,0 +1,78 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Grid.js
+// Dynamcially creates 2D grid for gameplay
+/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+// Imports
+/////////////////////////////////////////////////////////////////////////////////
+
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+
+/////////////////////////////////////////////////////////////////////////////////
+// Functions
+/////////////////////////////////////////////////////////////////////////////////
+
+// Create 2D Object with specified length
+const createGrid = (length) => {
+  let state = [], counter = 0;
+  for (let i = 0; i < length; i++) {
+    let temp = [];
+    for (let j = 0; j < length; j++) {
+      temp.push({ key: counter, state: 0 });
+      counter++;
+    }
+    state.push(temp);
+  }
+  return state;
+}
+
+// Return 2D grid of buttons
+const makeGrid = (length) => {
+  let state = createGrid(length);
+  return (
+    <View style={styles.col_container}>
+      {state.map(arr => {
+        return (
+          <View style={styles.row_container}>
+            {arr.map(element => {
+              return (
+                <View key={`${element.key}`}>
+                  <Button title={`${element.key}`} />
+                </View>
+              )
+            })}
+          </View>
+        )
+      })}
+    </View>
+  )
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+// Style
+/////////////////////////////////////////////////////////////////////////////////
+
+const styles = StyleSheet.create({
+  col_container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  row_container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+// Exports
+/////////////////////////////////////////////////////////////////////////////////
+
+// Export grid creator function
+export default makeGrid;
