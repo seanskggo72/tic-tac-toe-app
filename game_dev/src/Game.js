3e6468fce57faf6a978429d1e803d40b918da10d
Sean <seanskg72@gmail.com>
2021-01-07 18:51:00 +1100

    :ok_hand: style fixed and major game engine progress

--- game_dev/src/Game.js
+++ game_dev/src/Game.js
@@ -8,7 +8,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 import React from 'react';
-import { Text, View, StyleSheet, ImageBackground, Pressable } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground, Pressable, Dimensions } from 'react-native';
 import Game_background from './Svg_renderer';
 import { GameEngine } from "react-native-game-engine";
 
@@ -36,16 +36,18 @@ const createGrid = (length) => {
 // the buttons for that row in JSX
 const makeRow = (row, index) => {
   return (
-    <View style={styles.row_container} key={index}>
+    <View style={styles.row_render} key={index}>
       {row.map(element => {
         return (
-          <View key={String(element.key)}>
-            <Pressable style={styles.button} android_ripple={{ color: 'aqua' }}>
-              <ImageBackground source={require('../assets/circle.png')} style={styles.image}>
-                <Text style={styles.text}>{String(element.key)}</Text>
-              </ImageBackground>
-            </Pressable>
-          </View>
+          <Pressable
+            style={styles.button}
+            android_ripple={{ color: 'aqua' }}
+            key={String(element.key)}
+          >
+            <ImageBackground source={require('../assets/circle.png')} style={styles.image}>
+              <Text style={styles.text}>{String(element.key)}</Text>
+            </ImageBackground>
+          </Pressable>
         );
       })}
     </View>
@@ -53,24 +55,23 @@ const makeRow = (row, index) => {
 }
 
 // Return 2D grid of buttons
-const Game_screen = (length) => {
-  let state = createGrid(length);
+const Game_screen = () => {
   return (
-    <View style={styles.col_container}>
+    <View style={styles.main_container}>
       <Game_background style={{ position: 'relative' }} />
+      <View style={{ position: 'absolute' }}>
+        {createGrid(5)}
+      </View>
       <GameEngine
         entities={{
           1: {
-            particles: [40, 40],
+            particles: [0, 0],
             renderer: <Temp />
           }
         }}
         style={{ position: 'absolute' }}
       ></GameEngine>
-      <View style={{ position: 'absolute' }}>
-        {createGrid(5)}
-      </View>
-    </View >
+    </View>
   );
 }
 
@@ -95,31 +96,37 @@ const Temp = () => {
 // Style
 /////////////////////////////////////////////////////////////////////////////////
 
+const button_dimension = Dimensions.get('window').width * 0.15;
+
 const styles = StyleSheet.create({
-  col_container: {
+  // Aligns children in the centre
+  main_container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
   },
-  row_container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
+  // Makes child elements render in a row
+  row_render: {
+    flexDirection: 'row',
   },
+  // Styles each individual button element in grid
   button: {
+    borderRadius: 8,
     alignItems: "center",
+    justifyContent: 'center',
     backgroundColor: "#ffe419",
-    width: 50,
-    height: 50,
-    elevation: 25, // Android
+    aspectRatio: 1,
+    width: button_dimension,
+    height: button_dimension,
   },
+  // Image dimensions
   image: {
-    width: 50,
-    height: 50,
+    width: button_dimension,
+    height: button_dimension,
   },
+  // Temporary padding for text
   text: {
-    padding: 15,
+    padding: 18,
     textAlign: 'center',
   },
 });
