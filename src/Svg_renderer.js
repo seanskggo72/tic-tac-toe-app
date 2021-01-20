a9a4970c38b1203f17c6afc091dac4435d351716
Sean <seanskg72@gmail.com>
2021-01-21 00:06:12 +1100

    :tada: App complete

--- src/Svg_renderer.js
+++ src/Svg_renderer.js
@@ -11,6 +11,7 @@ import React from "react";
 import Svg, { Defs, Image, Path, Use } from "react-native-svg";
 import Background from '../svg/Game_background';
 import Home from '../svg/Home_background';
+import Instruction from '../svg/Instruction_background';
 
 /////////////////////////////////////////////////////////////////////////////////
 // Functions
@@ -66,9 +67,34 @@ const Home_background = (props) => {
   )
 }
 
+// Home Background SVG
+const Instruction_background = (props) => {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      preserveAspectRatio="none"
+      viewBox="0 0 810 1440"
+      {...props}
+    >
+      <Defs>
+        <Image
+          id="prefix__a"
+          width={2880}
+          height={1800}
+          xlinkHref={Instruction.instruction_background}
+        />
+      </Defs>
+      <Path fill="#fff" d="M0 0h810v1440H0z" />
+      <Path fill="#fff" d="M0 0h810v1440H0z" />
+      <Use xlinkHref="#prefix__a" transform="matrix(.8 0 0 .8 -747 0)" />
+    </Svg>
+  )
+}
+
 /////////////////////////////////////////////////////////////////////////////////
 // Exports
 /////////////////////////////////////////////////////////////////////////////////
 
-export { Game_background, Home_background };
+export { Game_background, Home_background, Instruction_background };
 
